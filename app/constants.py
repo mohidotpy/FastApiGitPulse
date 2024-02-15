7d3b5b70112bd474d5e3cb51be2ed8bd0f4fa42b
mohammad hasani <mohammad_hasani@live.com>
2024-02-15 12:47:30 +0100

    add constants

---
+++ app/constants.py
@@ -0,0 +1,11 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_TEST_DB = os.environ.get('POSTGRES_TEST_DB', 'GitPulseTestDB')
+
+PROD_DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+TEST_DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_TEST_DB}"
