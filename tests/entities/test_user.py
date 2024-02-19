6265e7606dac7694d18980500ef91ea9890d31f8
mohammad hasani <mohammad_hasani@live.com>
2024-02-19 19:16:11 +0100

    add test to check not create user object with invalid email address

--- tests/entities/test_user.py
+++ tests/entities/test_user.py
@@ -1,3 +1,6 @@
+import pytest
+
+from app.entities.exceptions import InvalidDataException
 from app.entities.user import User
 
 
@@ -5,3 +8,29 @@ def test_create_user_entity_with_valid_data():
     user_email = "mohidotpy@gmail.com"
     user = User(email=user_email)
     assert user.email == user_email
+
+
+def test_create_user_entity_with_invalid_email():
+    with pytest.raises(InvalidDataException) as e:
+        User(
+            email="mohidotpy_gmail.com"
+        )
+    assert e.value.message == 'Invalid email address'
+
+    with pytest.raises(InvalidDataException) as e:
+        User(
+            email="@.com"
+        )
+    assert e.value.message == 'Invalid email address'
+
+    with pytest.raises(InvalidDataException) as e:
+        User(
+            email="@gmail.com"
+        )
+    assert e.value.message == 'Invalid email address'
+
+    with pytest.raises(InvalidDataException) as e:
+        User(
+            email="mohidotpy@gmail."
+        )
+    assert e.value.message == 'Invalid email address'
