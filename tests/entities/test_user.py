222a38f6007c43f6ccea198aabd49f68b1ea770a
mohammad hasani <mohammad_hasani@live.com>
2024-02-19 18:51:49 +0100

    refactor it and add User entity to pass user entity test

--- tests/entities/test_user.py
+++ tests/entities/test_user.py
@@ -1,4 +1,7 @@
+from app.entities.user import User
+
+
 def test_create_user_entity_with_valid_data():
     user_email = "mohidotpy@gmail.com"
-    user = User(email="mohidotpy@gmail.com")
+    user = User(email=user_email)
     assert user.email == user_email
