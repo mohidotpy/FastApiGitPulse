5c39949c527875c1759e6293b33aa7b9ff0bcd7b
mohammad hasani <mohammad_hasani@live.com>
2024-02-25 12:39:27 +0100

    refactor test for external repo service

--- tests/services/test_repo_popularity_service.py
+++ tests/services/test_repo_popularity_service.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from app.core.container import Container
@@ -8,12 +10,13 @@ from app.core.container import Container
     (0, 5, False),
 ])
 def test_repo_popularity_service_work_correctly(star_number, fork_number, expected_is_popular):
-    external_git_repo_address = "https://github.com/mohidotpy/somthing"
+    external_git_repo_address = "https://github.com/something/somerepo"
+    external_git_service_instance = Container.external_git_service()
 
-    Container.external_git_service._repository.get_external_repo_information.return_value = (star_number, fork_number)
+    with patch('app.repository.external_git_repo_repository.ExternalGitRepoRepository.get_external_repo_information',
+               return_value=(star_number, fork_number)) as mock_get_info:
 
-    external_repo_popularity_information = Container.external_git_service.fetch_external_repo_popularity_information(
-        external_git_repo_address)
+        external_repo_popularity_information = external_git_service_instance.fetch_external_repo_popularity_information(
+            external_git_repo_address)
 
-    assert external_repo_popularity_information.score >= 500
-    assert external_repo_popularity_information.is_popular == expected_is_popular
+        assert external_repo_popularity_information.is_popular == expected_is_popular
