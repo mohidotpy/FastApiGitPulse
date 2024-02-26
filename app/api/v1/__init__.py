1285f8c768246063943b38d8fa36f3a38bfcce9a
mohammad hasani <mohammad_hasani@live.com>
2024-02-26 19:17:47 +0100

    add router v1 in package layer

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from app.api.v1.auth import auth_router
+from app.api.v1.github_checker import repo_router
+from app.api.v1.user import user_router
+
+v1_routers = APIRouter(prefix="/v1")
+router_list = [auth_router, user_router, repo_router]
+
+for r in router_list:
+    v1_routers.tags.append('v1')
+    v1_routers.include_router(r)
