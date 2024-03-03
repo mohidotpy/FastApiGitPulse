f3134a6af411437245b660171f17830a1e0a8579
mohammad hasani <mohammad_hasani@live.com>
2024-03-03 21:55:08 +0100

    add test for external git repo

--- tests/repositories/test_external_git_repo_repository.py
+++ tests/repositories/test_external_git_repo_repository.py
@@ -2,6 +2,7 @@ import json
 from unittest.mock import patch
 
 from app.core.container import Container
+from app.repository.exceptions import ExternalAPIError
 from tests.repositories.utils import generate_random_star_count_and_forks_count
 
 
@@ -34,3 +35,22 @@ def test_external_repo_information_store_successfully_in_cache(redis):
         json_data = json.loads(cache_data)
         assert json_data['stargazers_count'] == stargazers_count
         assert json_data['forks_count'] == forks_count
+
+
+def test_external_repo_information_raise_exception_in_case_of_error(redis):
+    redis_repository = Container.redis_repository(
+        redis_connection=redis,
+    )
+    owner = "someone"
+    repo = "somerepo"
+
+    external_git_repository = Container.external_git_repo_repository(access_token="fake_access_token",
+                                                                     cache_repo=redis_repository)
+
+    with patch("app.repository.external_git_repo_repository.requests.get") as mock_get:
+        mock_response = mock_get.return_value
+        mock_response.status_code = 401
+        try:
+            external_git_repository.get_cached_or_retrieve_external_repo_info(owner, repo)
+        except ExternalAPIError as e:
+            assert e.message == "Failed to fetch repository information"
