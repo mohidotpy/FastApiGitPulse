62129d3dbc55467c497bab41881234b8c9eeff88
mohammad hasani <mohammad_hasani@live.com>
2024-03-04 00:49:04 +0100

    add more asserts to test_external_repo_information_raise_exception_in_case_of_error

--- tests/repositories/test_external_git_repo_repository.py
+++ tests/repositories/test_external_git_repo_repository.py
@@ -1,6 +1,8 @@
 import json
 from unittest.mock import patch
 
+import pytest
+
 from app.core.container import Container
 from app.repository.exceptions import ExternalAPIError
 from tests.repositories.utils import generate_random_star_count_and_forks_count
@@ -37,7 +39,12 @@ def test_external_repo_information_store_successfully_in_cache(redis):
         assert json_data['forks_count'] == forks_count
 
 
-def test_external_repo_information_raise_exception_in_case_of_error(redis):
+@pytest.mark.parametrize("status_code, external_api_detail_error", [
+    (400, {"message": "Bad Request", "documentation_url": "https://docs.github.com"}),
+    (401, {"message": "Bad Credentials", "documentation_url": "https://docs.github.com"}),
+    (404, {"message": "Not Found", "documentation_url": "https://docs.github.com"})
+])
+def test_external_repo_information_raise_exception_in_case_of_error(redis, status_code,external_api_detail_error):
     redis_repository = Container.redis_repository(
         redis_connection=redis,
     )
@@ -49,8 +56,12 @@ def test_external_repo_information_raise_exception_in_case_of_error(redis):
 
     with patch("app.repository.external_git_repo_repository.requests.get") as mock_get:
         mock_response = mock_get.return_value
-        mock_response.status_code = 401
+        mock_response.status_code = status_code
+        mock_response.json.return_value = external_api_detail_error
+
         try:
             external_git_repository.get_cached_or_retrieve_external_repo_info(owner, repo)
         except ExternalAPIError as e:
             assert e.message == "Failed to fetch repository information"
+            assert e.status_code == status_code
+            assert e.external_api_detail_error == external_api_detail_error
