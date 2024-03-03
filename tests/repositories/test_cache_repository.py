ebe7f00dd52da0d9834f845cf76e91f9887b57ef
mohammad hasani <mohammad_hasani@live.com>
2024-03-03 21:31:54 +0100

    add test for cache repository

---
+++ tests/repositories/test_cache_repository.py
@@ -0,0 +1,67 @@
+import json
+from random import randint
+from unittest.mock import patch
+
+from app.core.config import setting
+
+CACHE_KEY = "repo:somone:somrepo"
+
+
+def generate_external_git_info_and_set_into_cache(redis_repo, stargazers_count, forks_count):
+    data = {
+        "stargazers_count": stargazers_count,
+        "forks_count": forks_count
+    }
+
+    return redis_repo.set(CACHE_KEY, json.dumps(data), ttl=setting.CACHE_DATA_TIME_TO_LIVE)
+
+
+def test_cache_repo_can_set_and_get_data_into_redis_successfully(client, container, redis):
+    stargazers_count = randint(0, 1000)
+    forks_count = randint(0, 1000)
+    redis_repo = container.redis_repository()
+
+    cache_data_from_redis = redis_repo.get(CACHE_KEY)
+    assert not cache_data_from_redis
+
+    generate_external_git_info_and_set_into_cache(redis_repo, stargazers_count, forks_count)
+
+    cache_data_from_redis = json.loads(redis_repo.get(CACHE_KEY))
+    assert cache_data_from_redis['stargazers_count'] == stargazers_count
+    assert cache_data_from_redis['forks_count'] == forks_count
+
+
+def test_get_non_existent_key_returns_none(client, container, redis):
+    redis_repo = container.redis_repository()
+    result = redis_repo.get(CACHE_KEY)
+    assert result is None
+
+
+def test_cache_repo_can_update_data_in_redis_successfully(client, container, redis):
+    initial_stargazers_count = randint(0, 1000)
+    initial_forks_count = randint(0, 1000)
+
+    redis_repo = container.redis_repository()
+
+    cache_data_from_redis = redis_repo.get(CACHE_KEY)
+    assert not cache_data_from_redis
+
+    generate_external_git_info_and_set_into_cache(redis_repo, initial_stargazers_count, initial_forks_count)
+    cache_data_from_redis = json.loads(redis_repo.get(CACHE_KEY))
+
+    assert cache_data_from_redis['stargazers_count'] == initial_stargazers_count, "Stargazers count was not created"
+    assert cache_data_from_redis['forks_count'] == initial_forks_count, "Forks count was not created"
+
+    updated_stargazers_count = randint(0, 1000)
+    updated_forks_count = randint(0, 1000)
+
+    generate_external_git_info_and_set_into_cache(redis_repo, updated_stargazers_count, updated_forks_count)
+    cache_data_from_redis = json.loads(redis_repo.get(CACHE_KEY))
+
+    assert cache_data_from_redis[
+               'stargazers_count'] == updated_stargazers_count, "Stargazers count was not updated correctly"
+    assert cache_data_from_redis['forks_count'] == updated_forks_count, "Forks count was not updated correctly"
+    assert cache_data_from_redis != {
+        "stargazers_count": initial_stargazers_count,
+        "forks_count": initial_forks_count
+    }, "Cache data should not match initial data after update"
