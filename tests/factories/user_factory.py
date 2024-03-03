fc8116c767adf61900663e4b8a056c464b6684a8
mohammad hasani <mohammad_hasani@live.com>
2024-03-03 22:25:04 +0100

    add factory for UserEntity

---
+++ tests/factories/user_factory.py
@@ -0,0 +1,44 @@
+import factory
+from sqlalchemy.orm import Session
+
+from app.core.security import get_password_hash, create_access_token
+from app.entities.user import User
+
+
+class UserEntityFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    id = factory.Sequence(lambda n: n)
+    email = factory.Faker('email')
+    password = factory.LazyAttribute(
+        lambda x: get_password_hash(factory.Faker('password', length=12).evaluate(None, None, {"locale": True})))
+    user_token = factory.Faker('uuid4')
+
+
+class UserFactory(UserEntityFactory, factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        sqlalchemy_session = None
+        sqlalchemy_session_persistence = "commit"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        logged_in = kwargs.pop('logged_in', False)
+        session = kwargs.pop('session', None)  # Extract the session from kwargs
+
+        if 'password' in kwargs:
+            plain_password = kwargs.pop('password')
+            hashed_password = get_password_hash(plain_password)
+            kwargs['password'] = hashed_password
+
+        if session:
+            assert isinstance(session, Session), "Provided session is not an instance of SQLAlchemy Session"
+            cls._meta.sqlalchemy_session = session
+
+        user = super(UserFactory, cls)._create(model_class, *args, **kwargs)
+
+        if logged_in:
+            access_token, expiration = create_access_token({'id': user.id, 'email': user.email})
+            user.access_token = access_token
+
+        return user
