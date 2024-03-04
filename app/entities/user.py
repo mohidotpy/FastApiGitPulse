b890cf07b1426fbf6da882852207f272d041e579
mohammad hasani <mohammad_hasani@live.com>
2024-03-04 01:08:22 +0100

    use INVALID_EMAIL_EXCEPTION_MESSAGE

--- app/entities/user.py
+++ app/entities/user.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import validates
 
 from app.entities import BaseEntity
+from app.entities.constants import INVALID_EMAIL_EXCEPTION_MESSAGE
 from app.entities.exceptions import InvalidDataException
 
 
@@ -16,5 +17,5 @@ class User(BaseEntity):
     def validate_email(self, key, email):
         pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
         if not re.match(pattern, email):
-            raise InvalidDataException(message="Invalid email address")
+            raise InvalidDataException(message=INVALID_EMAIL_EXCEPTION_MESSAGE)
         return email
