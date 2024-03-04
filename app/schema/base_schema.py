7912c1a2c39568386fd61db5422f01b2a266da68
mohammad hasani <mohammad_hasani@live.com>
2024-03-04 16:12:04 +0100

    move AllOptional to schema folder

--- app/schema/base_schema.py
+++ app/schema/base_schema.py
@@ -1,9 +1,22 @@
 from typing import Optional, Union
 
 from pydantic import BaseModel
+from pydantic.main import ModelMetaclass
 
 
 class FindBase(BaseModel):
     ordering: Optional[str]
     page: Optional[int]
     page_size: Optional[Union[int, str]]
+
+
+class AllOptional(ModelMetaclass):
+    def __new__(self, name, bases, namespaces, **kwargs):
+        annotations = namespaces.get("__annotations__", {})
+        for base in bases:
+            annotations.update(base.__annotations__)
+        for field in annotations:
+            if not field.startswith("__"):
+                annotations[field] = Optional[annotations[field]]
+        namespaces["__annotations__"] = annotations
+        return super().__new__(self, name, bases, namespaces, **kwargs)
