855f88cfe9ddc85b86f8bef8b8da7c2b7dcb4974
mohammad hasani <mohammad_hasani@live.com>
2024-03-04 16:33:43 +0100

    refactor auth test api

--- tests/test_api/test_auth_api.py
+++ tests/test_api/test_auth_api.py
@@ -4,15 +4,13 @@ from datetime import datetime
 from app.core.config import setting
 from app.entities.constants import INVALID_EMAIL_EXCEPTION_MESSAGE, INVALID_LOGIN_DATA_EXCEPTION_MESSAGE
 
-from tests.conftest import db, client, container
-
 from starlette import status
 
 from app.schema.user_schema import FindUserSchema
 from tests.factories.user_factory import UserFactory
 
 
-def test_register_user_api_with_correct_data(client, db, container):
+def test_register_user_api_with_correct_data(client, db_session, container):
     data = {"email": "mohidotpy@gmail.com", "password": "123456789"}
     res = client.post('/api/v1/auth/sign-up', json=data)
 
@@ -25,7 +23,7 @@ def test_register_user_api_with_correct_data(client, db, container):
     assert users['founds'][0].email == data['email']
 
 
-def test_register_user_api_with_bad_email_address(client, db):
+def test_register_user_api_with_bad_email_address(client, db_session, container):
     data = {"email": "mohidotpy@gmail", "password": "123456789"}
     res = client.post('/api/v1/auth/sign-up', json=data)
     res_body = res.json()
@@ -33,13 +31,11 @@ def test_register_user_api_with_bad_email_address(client, db):
     assert res_body['message'] == INVALID_EMAIL_EXCEPTION_MESSAGE
 
 
-def test_login_user_api_with_correct_data(client, db, container):
+def test_login_user_api_with_correct_data(client, db_session, container):
     email = "mohi@gmail.com"
     password = "123456789"
 
-    UserFactory._meta.sqlalchemy_session = db
-
-    UserFactory(email=email, password=password)
+    UserFactory(email=email, password=password, session=db_session)
     data = {"email": email, "password": password}
 
     res = client.post('/api/v1/auth/sign-in', json=data)
@@ -56,11 +52,11 @@ def test_login_user_api_with_correct_data(client, db, container):
                         abs_tol=margin_of_error_minutes), f"Expiration difference is outside the acceptable range: {total_diff_minutes} vs {setting.ACCESS_TOKEN_EXPIRE_MINUTES}"
 
 
-def test_login_user_api_with_incorrect_data(client, db, container):
+def test_login_user_api_with_incorrect_data(client, db_session, container):
     email = "mohi@gmail.com"
     password = "123456789"
 
-    UserFactory._meta.sqlalchemy_session = db
+    UserFactory._meta.sqlalchemy_session = db_session
 
     UserFactory(email=email, password=password)
     data = {"email": email, "password": "12345"}
