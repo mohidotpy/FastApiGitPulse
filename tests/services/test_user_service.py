fb4840322635f52fe37dc5244908a031ed0b1e09
mohammad hasani <mohammad_hasani@live.com>
2024-03-04 15:42:55 +0100

    add user_service test

---
+++ tests/services/test_user_service.py
@@ -0,0 +1,75 @@
+import math
+from datetime import datetime
+from unittest.mock import patch
+
+from app.core.config import setting
+from app.core.exceptions import AuthError
+from app.core.security import decode_jwt
+from app.entities.user import User
+from app.schema.auth_schema import SignUpSchema, SignInSchema
+from tests.factories.user_factory import UserEntityFactory
+
+PASSWORD = "S0meR4ndOmP!asworD987234"
+
+
+def test_user_service_get_by_id(container):
+    user_service_instance = container.user_service()
+    user_entity = UserEntityFactory(password=PASSWORD)
+    with patch('app.repository.user_repository.UserRepository.read_by_id') as mock_read_by_id:
+        mock_read_by_id.return_value = user_entity
+        user = user_service_instance.get_by_id(id=user_entity.id)
+
+        assert user.email == user_entity.email
+        assert user.id == user_entity.id
+        assert user.password == user_entity.password
+        assert not user.password == PASSWORD
+
+
+def test_user_service_get_list(container):
+    user_service_instance = container.user_service()
+    user_entity = UserEntityFactory(password=PASSWORD)
+    with patch('app.repository.user_repository.UserRepository.read_by_options') as mock_read_by_options:
+        mock_read_by_options.return_value = {
+            "founds": [user_entity],
+            'search_options': {'page': 1, 'page_size': 20, 'ordering': '-id', 'total_count': 1},
+        }
+
+        user = user_service_instance.get_list({"id": user_entity.id})
+        user = user['founds'][0]
+
+        assert user.email == user_entity.email
+        assert user.id == user_entity.id
+        assert user.password == user_entity.password
+        assert not user.password == PASSWORD
+
+
+def test_user_service_patch(container):
+    user_service_instance = container.user_service()
+    user_entity = UserEntityFactory(password=PASSWORD)
+    with patch('app.repository.user_repository.UserRepository.read_by_options') as mock_read_by_options:
+        mock_read_by_options.return_value = {
+            "founds": [user_entity],
+            'search_options': {'page': 1, 'page_size': 20, 'ordering': '-id', 'total_count': 1},
+        }
+
+        user = user_service_instance.get_list({"id": user_entity.id})
+        user = user['founds'][0]
+
+        assert user.email == user_entity.email
+        assert user.id == user_entity.id
+        assert user.password == user_entity.password
+        assert not user.password == PASSWORD
+
+
+def test_user_service_add(container):
+    user_service_instance = container.user_service()
+    user_entity = UserEntityFactory(password=PASSWORD)
+    with patch('app.repository.user_repository.UserRepository.create') as mock_read_by_options:
+        mock_read_by_options.return_value = user_entity
+
+        user = user_service_instance.add(user_entity)
+
+        assert user.email == user_entity.email
+        assert user.id == user_entity.id
+        assert user.password == user_entity.password
+        assert not user.password == PASSWORD
